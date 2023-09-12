b65f131c9088d14de5f256b30e1fd1970eaa8650
zoeque <121230485+zoeque@users.noreply.github.com>
2023-09-12 19:58:28 +0900

    #6 - dev, create simple form

--- front/limitchecker/src/App.tsx
+++ front/limitchecker/src/App.tsx
@@ -1,32 +1,57 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react';
+import DatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css'
 import './App.css'
 
 function App() {
+  const today = new Date();
   const [count, setCount] = useState(0)
+
+  const [inputItemName, setItemName] = useState('');
+  const [inputItemType, setItemType] = useState('');
+  const [inputExpiredDate, setExpiredDate] = React.useState(today);
+
+  const handleItemName = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setItemName(event.target.value);
+  };
+  const handleItemTypePulldown = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setItemType(event.target.value);
+  };
 
   return (
     <>
+      <h1>消費期限管理アプリケーション</h1>
+      <p>
+          購入した物を下記のフォームより登録してください。消費期限が近づくとメールにて通知します。
+        </p>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <input type="itemName" placeholder="対象商品名" value={inputItemName} onChange={handleItemName} />
       </div>
-      <h1>Vite + React</h1>
+      <div>
+        <select value={inputItemType} onChange={handleItemTypePulldown}>
+        /** TODO get paramters from CSV, XML, Database, etc... TBD implementation */
+          <option value="">選択してください</option>
+          <option value="オプション1">乳製品</option>
+          <option value="オプション2">肉類</option>
+          <option value="オプション3">調味料</option>
+        </select>
+      </div>
+      <div>
+        <DatePicker
+          selected={inputExpiredDate}
+          onChange={selectedDate => { setExpiredDate(selectedDate || today) }}
+          dateFormat="yyyy/MM/dd"
+          placeholderText="日付を選択"
+        />
+      </div>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Created by zoeque. See <a href="https://github.com/zoeque">GitHub</a> for a detail.
       </p>
     </>
   )
