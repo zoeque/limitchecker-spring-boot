075afbe64c061e304719490cad7fdef955db703c
zoeque <121230485+zoeque@users.noreply.github.com>
2023-09-14 19:09:39 +0900

    Task #6 - Modified the view of the screen

--- front/limitchecker/src/App.tsx
+++ front/limitchecker/src/App.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import DatePicker from 'react-datepicker'
 import 'react-datepicker/dist/react-datepicker.css'
 import './App.css'
+import Pulldown from './Pulldown';
 
 function App() {
   const today = new Date();
@@ -15,27 +16,18 @@ function App() {
   const handleItemName = (event: React.ChangeEvent<HTMLInputElement>) => {
     setItemName(event.target.value);
   };
-  const handleItemTypePulldown = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    setItemType(event.target.value);
-  };
 
   return (
     <>
       <h1>消費期限管理アプリケーション</h1>
       <p>
-          購入した物を下記のフォームより登録してください。消費期限が近づくとメールにて通知します。
-        </p>
+        購入した物を下記のフォームより登録してください。消費期限が近づくとメールにて通知します。
+      </p>
       <div>
         <input type="itemName" placeholder="対象商品名" value={inputItemName} onChange={handleItemName} />
       </div>
       <div>
-        <select value={inputItemType} onChange={handleItemTypePulldown}>
-        /** TODO get paramters from CSV, XML, Database, etc... TBD implementation */
-          <option value="">選択してください</option>
-          <option value="オプション1">乳製品</option>
-          <option value="オプション2">肉類</option>
-          <option value="オプション3">調味料</option>
-        </select>
+        <Pulldown />
       </div>
       <div>
         <DatePicker
@@ -47,7 +39,7 @@ function App() {
       </div>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          登録 {count}
         </button>
       </div>
       <p className="read-the-docs">
