139af99644396a481c96ca5f4652fd4b4832b58c
ry <121230485+zoeque@users.noreply.github.com>
2025-01-06 12:35:38 +0900

    update #25 - Controller for delete item

--- src/main/java/zoeque/limitchecker/adapter/StoredItemController.java
+++ src/main/java/zoeque/limitchecker/adapter/StoredItemController.java
@@ -4,21 +4,34 @@ import io.vavr.control.Try;
 import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import zoeque.limitchecker.application.dto.record.StoredItemJsonDto;
 import zoeque.limitchecker.application.service.usecase.StoredItemCreateService;
+import zoeque.limitchecker.application.service.usecase.StoredItemDropService;
 import zoeque.limitchecker.domain.entity.StoredItem;
 
 @RestController
 @CrossOrigin(origins = "*")
 @Component
 public class StoredItemController {
+  StoredItemCreateService service;
+
+  StoredItemDropService dropService;
+
   public StoredItemController(StoredItemCreateService service) {
     this.service = service;
   }
 
-  StoredItemCreateService service;
-
+  /**
+   * The method to create a new stored item.
+   *
+   * @param dto The target stored item to create.
+   * @return The response entity with the result of the creation process.
+   */
   @PostMapping("/create")
   public ResponseEntity createNewStoredItem(@RequestBody StoredItemJsonDto dto) {
     Try<StoredItem> createTry = service.createNewStoredItem(dto);
@@ -28,6 +41,11 @@ public class StoredItemController {
     return ResponseEntity.ok(dto);
   }
 
+  /**
+   * The method to find all stored items.
+   *
+   * @return The response entity with the list of stored items.
+   */
   @GetMapping("/find")
   public ResponseEntity findAllStoredItem() {
     Try<List<StoredItemJsonDto>> findTry = service.findAllStoredItem();
@@ -35,5 +53,20 @@ public class StoredItemController {
       return ResponseEntity.badRequest().body(findTry.getCause());
     }
     return ResponseEntity.ok(findTry.get());
+  }
+
+  /**
+   * The method to drop the stored item.
+   *
+   * @param dto The target stored item to drop.
+   * @return The response entity with the result of the drop process.
+   */
+  @PostMapping("/drop")
+  public ResponseEntity dropStoredItem(@RequestBody StoredItemJsonDto dto) {
+    Try<Long> dropTry = dropService.dropRequestedStoredItem(dto.storedItemIdentifier());
+    if (dropTry.isFailure()) {
+      return ResponseEntity.badRequest().body(dropTry.getCause());
+    }
+    return ResponseEntity.ok(dropTry.get());
   }
 }
