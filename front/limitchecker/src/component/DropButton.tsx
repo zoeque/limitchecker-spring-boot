8658b30d8ab714a69367097610abafb2d41b0a96
ry <121230485+zoeque@users.noreply.github.com>
2025-04-08 11:55:23 +0900

    update drop button

--- front/limitchecker/src/component/DropButton.tsx
+++ front/limitchecker/src/component/DropButton.tsx
@@ -1,21 +1,21 @@
 import { useState } from 'react';
 import './components.css'
-import '../controller/StoredItemController'
+import '../controller/StoredItemDropController'
 
 function DropButton({ 
-    storedItemIdentifier, itemName, itemType, expiredDate, sendPostRequest 
+    storedItemIdentifier, itemName, itemType, expiredDate, sendDropRequest 
 }:{
     storedItemIdentifier: Int16Array;
     itemName: string;
     itemType: string;
     expiredDate: string;
-    sendPostRequest: (storedItemIdentifier: Int16Array, itemName: string, itemType: string, expiredDate: string) => void;
+    sendDropRequest: (storedItemIdentifier: Int16Array, itemName: string, itemType: string, expiredDate: string) => void;
 }) {
     const [message, setMessage] = useState('');
     function clickButton() {
         try {
             {/** Send StoredItem as JSON after the validation */ }
-            sendPostRequest(storedItemIdentifier, itemName, itemType, expiredDate);
+            sendDropRequest(storedItemIdentifier, itemName, itemType, expiredDate);
             setMessage(itemName + 'を削除しました。')
         } catch (e) {
             setMessage("予期せぬエラーが発生しました。エラー：" + e);
